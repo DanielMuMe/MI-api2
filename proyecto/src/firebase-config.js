562cd0e60bdea16464ad213fb5741ad76dd18ba4
DanielMuMe <daniel.murillom@uniagustiniana.edu.co>
2025-05-26 00:10:16 +0000

    fire datos

--- proyecto/src/firebase-config.js
+++ proyecto/src/firebase-config.js
@@ -8,12 +8,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js ";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBNyuUCfRwTv01e54l6-vNfjyt0jCHbnQ8",
+  authDomain: "fireapi-219c9.firebaseapp.com",
+  projectId: "fireapi-219c9",
+  storageBucket: "fireapi-219c9.firebasestorage.app",
+  messagingSenderId: "509515453815",
+  appId: "1:509515453815:web:3847dbb848cad260d27d72"
 };
 
 const app = initializeApp(firebaseConfig);
